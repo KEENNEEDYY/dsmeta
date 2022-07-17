b4fdc97c54da7608d91412799761b220b8e58e74
KENNEDY COSTA <kennedy.costa.valenet@gmail.com>
2022-07-17 14:23:36 -0300

    Sale listing

---
+++ frontend/src/components/models/sale.ts
@@ -0,0 +1,11 @@
+    
+    
+    export type Sale = {
+        id: number;
+        date: number;
+        sellerName: string;
+        visited: number;
+        deals: number;
+        amount: number;
+
+    }
